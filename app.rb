2bbf3a6688b4b68c245ff4b724161c8b70721e4a
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2024-05-13 17:55:59 +0000

    square with form function done

--- app.rb
+++ app.rb
@@ -1,6 +1,30 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  
+erb(:hello)  
+  
+end
+
+get("/goodbye") do
+  
+  erb(:bye)  
+    
+  end
+
+  get("/square/new") do
+    erb(:new_square_calc)
+  end
+
+
+  get '/square/result' do
+    @the_num = params.fetch("number").to_f
+    @the_result =@the_num **2
+
+    erb(:square_results)
+  end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
