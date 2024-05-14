7b13ff640d02ca75fdc56271405e373bdb9d37fe
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2024-05-14 18:54:15 +0000

    working on payment result

--- app.rb
+++ app.rb
@@ -38,6 +38,32 @@ get("/goodbye") do
     erb(:square_root_results)
   end
 
+  get '/payment/new' do
+    erb(:new_payment_calc)
+  end
+
+  get '/payment/result' do
+    @the_apr = params.fetch("apr").to_f/100/12
+    @the_years = params.fetch("years").to_i * 12
+    @the_principal = params.fetch("principal").to_f
+
+    numerator = @the_apr *  @the_principal
+    denominator = 1 - (1 + @the_apr) ** -@the_years
+    @the_payment = (numerator / denominator).round(2)
+
+
+
+  end
+
+  apr = params[:apr].to_f / 100 / 12
+  n = params[:years].to_i * 12
+  pv = params[:present_value].to_f
+  
+  numerator = apr * pv
+  denominator = 1 - (1 + apr) ** -n
+  @payment = (numerator / denominator).round(2)
+  @interest_rate = (apr * 12 * 100).round(4)
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
